22a4d7b44068eb1f5a50442799c8c1dcf231b283
nishendra9 <snishendra@sudent.ethz.ch>
2022-05-02 00:41:19 +0200

    added lyrics display

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Spinner from '../layout/Spinner'
 
 function withMyHook(Component) {
@@ -35,7 +35,33 @@ class Lyrics extends Component {
         {
             return <Spinner></Spinner>
         } else {
-            return <h1>Data Returned</h1>
+            console.log(track);
+            return (
+                <React.Fragment>
+                    <Link to="/" className="btn btn-dark btn-sm mb-4"> Go Back </Link>
+                    <div className="card">
+                        <h5 className="card-header">
+                            {track.track_name} by <span className='text-secondary'>{track.artist_name}</span>
+                        </h5>
+                        <div className="card-body">
+                            <p className="card-text">{lyrics.lyrics_body}</p>
+                        </div>
+                    </div>
+
+                    <ul className="list-group mt-3">
+                        <li className="list-group-item">
+                            <strong>Album ID</strong>: {track.album_id}  
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Song Genre</strong>: {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}  
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Explicit Words</strong>: {track.explicit === 0 ? 'No' : 'Yes'}
+                        </li>
+                    </ul>
+                </React.Fragment>
+
+            )
         }
          
     }
